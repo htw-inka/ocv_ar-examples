158a34b72c94d4b3ab674e1cf0f505071d20b570
Markus Konrad <post@mkonrad.net>
2014-06-19 10:55:03 +0200

    added empty new project OcvARBasic for iOS

---
+++ examples/ios/OcvARBasic/OcvARBasic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OcvARBasic
+//
+//  Created by Markus Konrad on 19.06.14.
+//  Copyright (c) 2014 INKA Research Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/ios/OcvARBasic/OcvARBasicTests/OcvARBasicTests.m
@@ -0,0 +1,34 @@
+//
+//  OcvARBasicTests.m
+//  OcvARBasicTests
+//
+//  Created by Markus Konrad on 19.06.14.
+//  Copyright (c) 2014 INKA Research Group. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OcvARBasicTests : XCTestCase
+
+@end
+
+@implementation OcvARBasicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
