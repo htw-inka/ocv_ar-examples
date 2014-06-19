edcf5b60cf4d75ec03b0f789e078d84d364f9ff0
Markus Konrad <post@mkonrad.net>
2014-06-19 16:36:43 +0200

    added code doc

--- examples/ios/OcvARBasic/OcvARBasic/AppDelegate.m
+++ examples/ios/OcvARBasic/OcvARBasic/AppDelegate.m
@@ -1,4 +1,13 @@
-
+/**
+ * OcvARBasic - Basic ocv_ar example for iOS
+ *
+ * Main app delegate - implementation file.
+ *
+ * Author: Markus Konrad <konrad@htw-berlin.de>, June 2014.
+ * INKA Research Group, HTW Berlin - http://inka.htw-berlin.de/
+ *
+ * BSD licensed (see LICENSE file).
+ */
 
 #import "AppDelegate.h"
 
--- examples/ios/OcvARBasic/OcvARBasic/GLView.h
+++ examples/ios/OcvARBasic/OcvARBasic/GLView.h
@@ -1,3 +1,13 @@
+/**
+ * OcvARBasic - Basic ocv_ar example for iOS
+ *
+ * gl view - header file.
+ *
+ * Author: Markus Konrad <konrad@htw-berlin.de>, June 2014.
+ * INKA Research Group, HTW Berlin - http://inka.htw-berlin.de/
+ *
+ * BSD licensed (see LICENSE file).
+ */
 
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
@@ -8,30 +18,37 @@
 using namespace cv;
 using namespace std;
 
+/**
+ * GLView highlights the found markers according to their estimated 3D pose.
+ * It sits above the camera frame view and is not opaque.
+ */
 @interface GLView : GLKView {
     BOOL glInitialized;
     
-    Shader markerDispShader;
+    Shader markerDispShader;    // marker display shader
     
-    GLint shAttrPos;
+    GLint shAttrPos;            // shader attribute: vertex data
     
-    GLint shMarkerProjMat;
-    GLint shMarkerModelViewMat;
-    GLint shMarkerTransformMat;
-    GLint shMarkerColor;
+    GLint shMarkerProjMat;      // shader uniform: projection matrix
+    GLint shMarkerModelViewMat; // shader uniform: model-view matrix
+    GLint shMarkerTransformMat; // shader uniform: transform matrix
+    GLint shMarkerColor;        // shader uniform: marker color
     
-    CGSize viewportSize;
+    CGSize viewportSize;        // real gl viewport size in pixels
     
-    GLfloat markerScaleMat[16];
+    GLfloat markerScaleMat[16]; // global marker transform (scale) matrix
 }
 
-@property (nonatomic, assign) vector<ocv_ar::Marker *> markers;
-@property (nonatomic, assign) float *markerProjMat;   // 4x4 projection matrix
-@property (nonatomic, assign) float markerScale;
-@property (nonatomic, assign) BOOL showMarkers;
+@property (nonatomic, assign) vector<ocv_ar::Marker *> markers; // found markers
+@property (nonatomic, assign) float *markerProjMat;             // 4x4 projection matrix
+@property (nonatomic, assign) float markerScale;                // marker scaling
+@property (nonatomic, assign) BOOL showMarkers;                 // enable/disable marker display
 
-- (void)setMarkerScale:(float)s;
+- (void)setMarkerScale:(float)s;    // overwrite 'assign' method
 
+/**
+ * Resize the gl view and adjust gl properties
+ */
 - (void)resizeView:(CGSize)size;
 
 @end
