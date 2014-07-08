1dab0d9e225535cd9e5059bd8cb557639c77c436
Markus Konrad <post@mkonrad.net>
2014-07-08 14:54:13 +0200

    added empty cocos2d project

---
+++ examples/ios/OcvARCocos2D/OcvARCocos2D/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  OcvARCocos2D
+//
+//  Created by Markus Konrad on 08.07.14.
+//  Copyright INKA Research Group 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ examples/ios/OcvARCocos2D/OcvARCocos2D/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OcvARCocos2D
+//
+//  Created by Markus Konrad on 08.07.14.
+//  Copyright INKA Research Group 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
